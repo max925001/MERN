69b4a523ff5cb7de24c90304b742c9222c36bbc0
max925001 <shivam969684@gmail.com>
2024-03-17 11:38:54 -0700

    initial commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,52 @@
-import { useState } from 'react'
+
 
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+import Home from './Pages/Home'
+import AboutUs from './Pages/AboutUs'
+import NotFound from './Pages/NotFound'
+import Signup from './Pages/Signup'
+import Login from './Pages/Login'
+import CourseList from './Pages/course/CourseList'
+import Contact from './Pages/Contact'
+import Dienied from './Pages/Dienied'
+import CourseDes from './Pages/course/CourseDes'
+import RequiredAuth from './Components/Auth/RequiredAuth'
+import CreateCourse from './Pages/course/CreateCourse'
+import Profile from './Pages/User/Profile'
 
 function App() {
- 
+  
 
   return (
     <>
-     hello
+   
+
+      <Routes>
+
+
+<Route  path='/' element={<Home/>}/>
+<Route  path='/about' element={<AboutUs/>}/>
+<Route path='/signup' element={<Signup/>}/>
+<Route  path='/login' element={<Login/>}/>
+
+<Route path='/courses' element={<CourseList/>}/>
+<Route path='/contact' element={<Contact/>}/>
+<Route path='/course/description' element={<CourseDes/>}/>
+<Route  element={<RequiredAuth allowedRoles={["ADMIN"]}/>}>
+
+<Route path='/course/create' element={<CreateCourse/>}/>
+
+</Route>
+<Route element={<RequiredAuth allowedRoles={["ADMIN" ,"USER"]}/>}>
+<Route path='/user/profile' element={<Profile/>}/>
+</Route>
+<Route path='/denied' element={<Dienied/>}/>
+<Route path='*' element={<NotFound/>}/>
+
+     </Routes>
+  
     </>
   )
 }
---
+++ vite-project/src/Redux/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from './Slices/AuthSlice'
+import courseSliceReducer from "./Slices/courseSlice";
+
+const store = configureStore({
+    reducer: {
+        auth: authSliceReducer,
+        course: courseSliceReducer
+        
+
+    },
+    devTools: true
+})
+
+export default store
